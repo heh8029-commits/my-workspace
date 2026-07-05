78f81736fc2d014b73ed6f141d804d5c3838e26b
heh8029-commits <heh8029@gmail.com>
2026-07-06 00:10:04 +0900

    투자기준 계산기 추가 - 현재자산정보 목표비중 시뮬레이터 하단

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'urij-v137';
+﻿const CACHE_NAME = 'urij-v138';
 const ASSETS = [
   './index.html', './hee.html', './work.html', './husband.html',
   './manifest.json', './manifest-hee.json', './manifest-husband.json',
